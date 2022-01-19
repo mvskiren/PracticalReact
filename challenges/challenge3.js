ae7612c2c206004134b975a4931f3fc97f6b8284
MvsKiran <kiranmvs07@gmail.com>
2022-01-19 23:00:07 +0530

    Update and rename Challenge3.js to challenge3.js

--- challenges/challenge3.js
+++ challenges/challenge3.js
@@ -42,3 +42,41 @@ function App() {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+---------------------------------------------------------------------------------------- Solutions 2 
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function Wait ({ delay = 1000, placeholder, ui }) {
+  const [show, setShow] = React.useState(false)
+
+  React.useEffect(() => {
+    const id = window.setTimeout(() => {
+      setShow(true)
+    }, delay)
+
+    return () => window.clearTimeout(id)
+  }, [delay])
+
+  return show === true
+    ? ui
+    : placeholder
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Wait
+        delay={3000}
+        placeholder={<p>Waiting...</p>}
+        ui={<p>This text should appear after 3 seconds.</p>}
+      />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
